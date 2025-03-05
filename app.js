c702789268f86ffe4f70219b755b3e1e43f36627
richardwfar <rwallce9@gmail.com>
2025-03-04 22:48:51 -0300

    Visualização dos usuários no HTML

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 import express from "express"
+import path from "path"
+import { dirname } from "path"
+import { fileURLToPath } from "url"
 import routes from "./src/routes.js"
 
 const app = express()
 
-app.use(express.urlencoded({ extended: true }))
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, ".", "src", "views"))
+
+app.use(express.static(path.join(__dirname, ".", "src", "public")))
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.use(routes)
 
 app.listen(3333, () => {
--- src/controllers/users/getUsersController.js
+++ src/controllers/users/getUsersController.js
@@ -3,7 +3,7 @@ import { prisma } from "../../models/prismaClient.js"
 export const getUsers = async (req, res) => {
     try {
         const users = await prisma.user.findMany()
-        res.status(200).json(users)
+        res.render("users", { users })
     } catch(error) {
         res.status(500).json({ error:"Erro ao buscar usuários" })
     }
